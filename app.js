44c95214c774285428dfaeaa674dcafbfe5cd8a2
venky <chennarapuvenkateswarareddy>
2024-04-04 02:29:01 +0000

    js

---
+++ app.js
@@ -0,0 +1,9 @@
+let express = require('express')
+let app = express()
+
+app.get('/gadgets', (request, response) => {
+  response.sendFile('./gadgets.html', {root: __dirname})
+})
+app.listen(3000)
+
+module.exports = app
